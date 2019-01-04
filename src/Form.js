0d75374b5f549d1714872860f12e7f42d5302e05
José Anibal Carneiro Escalante <jcarneiro@belatrix.com>
2019-01-04 10:08:33 -0500

    edit App.css and Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,51 +1,82 @@
-import React from 'react';
+import React from "react";
 
-export default class Form extends React.Component{
-    state={
+export default class Form extends React.Component {
+  state = {
+    fullname: "",
+    position: "",
+    email: "",
+    skypeID: "",
+    emailService: ""
+  };
 
-        fullname:'',
-        position:'',
-        email:'',
-        skypeID:'',
-        emailService:''
-    }
-
-    render(){
-        return(
-        <div className="row row-form">
+  render() {
+    return (
+      <div className="row row-form">
         <div className="title">
-        <h2 className="signature-generator">Signature Generator</h2>
+          <h2 className="signature-generator">Signature Generator</h2>
         </div>
-        
-        <h6 className="fill-in-your-informa">Fill in your information to generate your email signature</h6>
-        <form className="col s12">
-        <div className="row">
-        <div className="input-field col s12">
-        <input placeholder="Enter your full name" id="first_name" type="text" className="validate"/>
-        <label id="word" className="active word" for="first_name">Full Name *</label>
-        </div>
-        </div>
-        <div className="row">
-        <div className="input-field col s12">
-        <input placeholder="Type or select your position" id="first_name" type="text" className="validate"/>
-        <label className="active" for="first_name">Position *</label>
-        </div>
-        </div>
-        <div className="row">
-        <div className="input-field col s12">
-        <input placeholder="email@belatrixsf.com" id="first_name" type="text" className="validate"/>
-        <label className="active" for="first_name">Email *</label>
-        </div>
-        </div>
-        <div className="row">
-        <div className="input-field col s12">
-        <input placeholder="Enter your Skype ID" id="first_name" type="text" className="validate"/>
-        <label className="active" for="first_name">Skype ID *</label>
-        </div>
-        </div>
+
+        <h6 className="fill-in-your-informa">
+          Fill in your information to generate your email signature
+        </h6>
+
+        <form className="asd col s12 pull-s1">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                placeholder="Enter your full name"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+              <label id="word" className="active word" for="first_name">
+                Full Name *
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                placeholder="Type or select your position"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+              <label className="active" for="first_name">
+                Position *
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                placeholder="email@belatrixsf.com"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+              <label className="active" for="first_name">
+                Email *
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                placeholder="Enter your Skype ID"
+                id="first_name"
+                type="text"
+                className="validate"
+              />
+              <label className="active" for="first_name">
+                Skype ID *
+              </label>
+            </div>
+          </div>
         </form>
-        </div>
-            
-        );
-    }
+
+
+      </div>
+    );
+  }
 }
