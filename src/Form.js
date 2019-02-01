bf685d9cad2512981807bd5325a9fcc67963a86f
José Anibal Carneiro Escalante <jcarneiro@belatrix.com>
2019-02-01 14:58:21 -0500

    correction of the modal issue

--- src/Form.js
+++ src/Form.js
@@ -1,8 +1,10 @@
 import React from "react";
+import $ from "jquery";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import Select from "react-select";
 import CopyContentButton from "./CopyContentButton";
+import M from 'materialize-css'
 
 const options = [
   { value: "UX Designer", label: "UX Designer" },
@@ -20,6 +22,12 @@ const options = [
   { value: "Office Manager", label: "Office Manager" },
   { value: "RRHH", label: "RRHH" }
 ];
+/* const locationOptions = [
+  { value: "1111111111", label: "Perú" },
+  { value: "2222222222", label: "Colombia" },
+  { value: "3333333333", label: "Argentina" },
+  { value: "4444444444", label: "USA" }
+]; */
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -27,18 +35,28 @@ export default class Form extends React.Component {
     this.state = {
       /* === To-Do : little hack to make placeholder prop work === */
       //value: '',
-      fullName: '',
-      position: { label: '', value: '' },
-      email: '',
-      title: '<b>Belatrix Software</b>',
-      skypeId: '',
-      emailService: ''
+      fullName: "",
+      position: { label: "", value: "" },
+      email: "",
+      title: "<b>Belatrix Software</b>",
+      skypeId: "",
+      emailService: "",
+      phoneNumbers:""
     };
   }
 
   handleSubmit = event => {
+
+    // Se llama después de que el navegador ya valida los required, etc...
     event.preventDefault();
-    const data = this.state;
+
+    const buttonMaterializeInstance = M.Modal.getInstance(this.modal.current)
+    const validoAdicional = true;
+    if (validoAdicional) {
+
+      buttonMaterializeInstance.open()
+    }
+    /* const data = this.state; */
   };
 
   handleInputChange = event => {
@@ -58,6 +76,16 @@ export default class Form extends React.Component {
     /* let obj ={value: position.value, label: position.label};
     this.setState({value: obj}); */
   };
+
+  toggleCheckbox = number =>{
+   console.log(number.value);
+
+   /* this.componentDidMount(){
+     $("#personalPhoneInput").show()
+    } */
+  };
+
+  modal = React.createRef()
 
   render() {
     const { fullName, position } = this.state;
@@ -106,40 +134,46 @@ export default class Form extends React.Component {
     };
 
     return (
-      <div className="row main">
-        <div className="title">
-          <h2 className="signature-generator">Signature Generator</h2>
+      <div className='row main'>
+        <div className='title'>
+          <h2 className='signature-generator'>Signature Generator</h2>
         </div>
-        <div className="divider"/>
-        <div id="subtitle" className="subtitle">
-          <h6 className="fill-in-your-informa">
+        <div className='divider' />
+        <div id='subtitle' className='subtitle'>
+          <h6 className='fill-in-your-informa'>
             Fill in your information to generate your email signature
           </h6>
         </div>
 
-        <form onSubmit={this.handleSubmit} className="col s12" id="myForm">
-          <div className="row input">
-            <div className="input-field col s12">
+        <form
+          id='myForm'
+          ref='form'
+          onSubmit={this.handleSubmit}
+          className='col s12'
+          id='myForm'
+        >
+          <div className='row input'>
+            <div className='input-field col s12'>
               <input
                 required
-                placeholder="Enter your full name"
-                id="full_name"
-                name="fullName"
-                type="text"
-                className="validate"
+                placeholder='Enter your full name'
+                id='full_name'
+                name='fullName'
+                type='text'
+                className='validate'
                 onChange={this.handleInputChange}
               />
-              <label className="active" htmlFor="first_name">
+              <label className='active' htmlFor='first_name'>
                 Full Name *
               </label>
             </div>
           </div>
-          <div className="row input">
-            <div className="input-field col s12">
+          <div className='row input'>
+            <div className='input-field col s12'>
               <Select
                 styles={colourStyles}
                 required
-                name="position"
+                name='position'
                 value={this.state.position}
                 onChange={this.handleChange}
                 options={options}
@@ -147,76 +181,117 @@ export default class Form extends React.Component {
                 //value={this.state.value}
               />
 
-              <label className="active" htmlFor="position" id="word">
+              <label className='active' htmlFor='position' id='word'>
                 Position *
               </label>
             </div>
           </div>
-          <div className="row input">
-            <div className="input-field col s12">
+          <div className='row input'>
+            <div className='input-field col s12'>
               <input
-                placeholder="email@belatrixsf.com"
-                id="email"
-                type="email"
-                className="validate"
-                name="email"
+                placeholder='email@belatrixsf.com'
+                id='email'
+                type='email'
+                className='validate'
+                name='email'
                 onChange={this.handleInputChange}
               />
               <label
-                className="active"
-                htmlFor="email"
-                data-error="Please add @"
-                required="required"
-                aria-required="true"
+                className='active'
+                htmlFor='email'
+                data-error='Please add @'
+                required='required'
+                aria-required='true'
               >
                 Email *
               </label>
             </div>
           </div>
-          <div className="row input">
-            <div className="input-field col s12">
+          <div className='row input'>
+            <div className='input-field col s12'>
               <input
-                placeholder="Enter your Skype ID"
-                id="skypeId"
-                name="skypeId"
-                type="text"
-                className="validate"
+                placeholder='Enter your Skype ID'
+                id='skypeId'
+                name='skypeId'
+                type='text'
+                className='validate'
                 onChange={this.handleInputChange}
               />
-              <label className="active" htmlFor="skypeId">
+              <label className='active' htmlFor='skypeId'>
                 Skype ID *
               </label>
             </div>
           </div>
 
-          <div className="row email-services-section">
-            <div className="col s12">
-              <div className="row">
-                <div className="col s12">
-                  <span className="email-service-span">
+          <div className='row input'>
+            <div className='input-field col s12'>
+              <p>
+                <label>
+                  <input type='checkbox' />
+                  <span>Perú</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <input type='checkbox' />
+                  <span>Colombia</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <input type='checkbox' />
+                  <span>Argentina</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <input type='checkbox'/>
+                  <span>USA</span>
+                </label>
+              </p>
+              <p>
+                <label>
+                  <input id="personalNumber" value="98789798" type='checkbox' checked={this.state.active} className="filled-in" onChange={this.toggleCheckbox}/>
+                  <span>Personal</span>
+                </label>
+              </p>
+              {/* input to show for personal phone numbers when personal checkbox is checked */}
+              {/* <input id="personalPhoneInput" type="text" hidden/> */}
+
+              <label className='active' htmlFor='skypeId'>
+                Add phone number from:
+              </label>
+            </div>
+          </div>
+
+          <div className='row email-services-section'>
+            <div className='col s12'>
+              <div className='row'>
+                <div className='col s12'>
+                  <span className='email-service-span'>
                     Available for these email providers:
                   </span>
                 </div>
               </div>
-              <div className="email-service-container">
-                <div className="row-buttons">
-                  <div className="col s12" onChange={this.handleInputChange}>
-                    <div className="rectangle">
-                      <span className="btn-flat no-cursor">
-                        <img src={require("./gmail.png")} alt="" />
-                        <span className="button-text">Gmail</span>
+              <div className='email-service-container'>
+                <div className='row-buttons'>
+                  <div className='col s12' onChange={this.handleInputChange}>
+                    <div className='rectangle'>
+                      <span className='btn-flat no-cursor'>
+                        <img src={require("./gmail.png")} alt='' />
+                        <span className='button-text'>Gmail</span>
                       </span>
                     </div>
-                    <div className="rectangle">
-                      <span className="btn-flat no-cursor">
-                        <img src={require("./outlook.png")} alt="" />
-                        <span className="button-text">Outlook</span>
+                    <div className='rectangle'>
+                      <span className='btn-flat no-cursor'>
+                        <img src={require("./outlook.png")} alt='' />
+                        <span className='button-text'>Outlook</span>
                       </span>
                     </div>
-                    <div className="rectangle">
-                      <span className="btn-flat no-cursor">
-                        <img src={require("./roundcube.png")} alt="" />
-                        <span className="button-text">Roundcube</span>
+                    <div className='rectangle'>
+                      <span className='btn-flat no-cursor'>
+                        <img src={require("./roundcube.png")} alt='' />
+                        <span className='button-text'>Roundcube</span>
                       </span>
                     </div>
                   </div>
@@ -224,30 +299,31 @@ export default class Form extends React.Component {
               </div>
             </div>
           </div>
-          <div className="button-final-container">
+          <div className='button-final-container'>
             <button
-              className="button-final btn-flat modal-trigger"
-              type="submit"
-              name="action"
-              data-target="modal1"
+              className='button-final btn-flat modal-trigger'
+              type='submit'
+              name='action'
+              
+              /* data-target='modal1' */
             >
               GENERATE SIGNATURE
             </button>
           </div>
         </form>
 
-        <div id="modal1" className="modal">
-          <div className="modal-content">
-            <img id="img-confirm" src={require("./confirmation.png")} alt="" />
+        <div id='modal1' className='modal' ref={this.modal}>
+          <div className='modal-content'>
+            <img id='img-confirm' src={require("./confirmation.png")} alt='' />
 
-            <h2 className="signature-generated">Signature Generated</h2>
-            <h4 className="subtitle">
+            <h2 className='signature-generated'>Signature Generated</h2>
+            <h4 className='subtitle'>
               Just copy the information below to add it to your email
             </h4>
           </div>
-          <div className="modal-footer">
-            <div className="footer-cage">
-              <div className="footer-cage-text">
+          <div className='modal-footer'>
+            <div className='footer-cage'>
+              <div className='footer-cage-text'>
                 {fullName} {position.label}...
               </div>
             </div>
@@ -257,8 +333,8 @@ export default class Form extends React.Component {
             </div>
           </div>
         </div>
-        <div className="fixed">
-          <div className="copied-to-clipboard">COPIED TO CLIPBOARD</div>
+        <div className='fixed'>
+          <div className='copied-to-clipboard'>COPIED TO CLIPBOARD</div>
         </div>
       </div>
     );
