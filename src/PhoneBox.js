00cfa7519c8837ebcecb5cba4d6937c61a3a5d9c
José Anibal Carneiro Escalante <jcarneiro@belatrix.com>
2019-02-04 15:13:16 -0500

    add component PhoneBox to be use as a display of the aditional phone numbers

---
+++ src/PhoneBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default class PhoneBox extends React.Component{
+    constructor(props){
+        
+    }
+    render(){
+        return(
+            <div></div>
+
+        );
+
+        
+    }
+}
