13cabcd35378006663c4d6314593f35c7ef0a0ff
José Anibal Carneiro Escalante <jcarneiro@belatrix.com>
2019-02-14 15:29:18 -0500

    applying styles to phoneBox component

--- src/PhoneBox.js
+++ src/PhoneBox.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'flag-icon-css/css/flag-icon.css';
+import 'flag-icon-css/css/flag-icon.min.css';
 export default class PhoneBox extends React.Component {
   render() {
+
+  
+
     return (
       <div className="select-validate phone-box">
         {this.presentNumbers(this.props.userData)}
@@ -8,22 +15,33 @@ export default class PhoneBox extends React.Component {
     );
   }
 
+  
+
   presentNumbers(phoneNumbers) {
+    var aux = '';
+    
+
     return (
       <React.Fragment>
-        <center>
-          <span>
-            <u>Phones to be add</u>:
-          </span>
-        </center>
-        <br />
-        {phoneNumbers.map((item, i) => (
-          <React.Fragment key={i}>
-            {item.location.value} Phone:{' '}
-            <a href={`tel:${item.phone}`}>{item.phone}</a>
-            <br />
-          </React.Fragment>
-        ))}
+        <ul className="collection">
+          <li className="collection-item avatar">
+            {phoneNumbers.map((item, i) => (
+              <React.Fragment key={i}>
+                {
+                  {
+                    'Argentina': (aux = 'gr'),
+                    'Perú': (aux = 'pe'),
+                    default: 'asd'
+                  }[item.location.value]
+                }
+                <span className={`flag-icon flag-icon-${aux}`}></span>
+                {item.location.value} Phone:{' '}
+                <a href={`tel:${item.phone}`}>{item.phone}</a>
+                <br />
+              </React.Fragment>
+            ))}
+          </li>
+        </ul>
       </React.Fragment>
     );
   }
